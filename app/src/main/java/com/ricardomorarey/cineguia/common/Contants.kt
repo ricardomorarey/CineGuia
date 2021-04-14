beb197d0a3623df854e950eed4ad536fdb237a96
Ricardo <pro1@bydemes.com>
2021-04-14 16:05:19 +0200

    Add PopularMoviesResponse.kt

--- app/src/main/java/com/ricardomorarey/cineguia/common/Contants.kt
+++ app/src/main/java/com/ricardomorarey/cineguia/common/Contants.kt
@@ -2,6 +2,8 @@ package com.ricardomorarey.cineguia.common
 
 class Contants {
     companion object{
-        public val CINE_BASE_URL = ""
+        public val CINE_BASE_URL = "https://api.themoviedb.org/3/"
+        public val APY_KEY = "4be0f096ab230819e117c61a1b43938b"
+        public val IMAGE_BASE_URL  = "https://image.tmdb.org/t/p/w500"
     }
 }
