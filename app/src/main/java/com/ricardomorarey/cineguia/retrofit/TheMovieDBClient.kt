19210ab3d9b8953b18a4b218f2c86205eb5427a3
Ricardo <pro1@bydemes.com>
2021-04-29 19:24:58 +0200

    Repository

---
+++ app/src/main/java/com/ricardomorarey/cineguia/retrofit/TheMovieDBClient.kt
@@ -0,0 +1,45 @@
+package com.ricardomorarey.cineguia.retrofit
+
+import com.google.gson.Gson
+import com.ricardomorarey.cineguia.common.Constants
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+
+class TheMovieDBClient {
+
+    private val theMovieDBService: TheMovieDBService
+    private val retrofit: Retrofit
+
+    companion object {
+        var instance: TheMovieDBClient? = null
+            get() {
+                if (field == null) {
+                    instance = TheMovieDBClient()
+                }
+                return instance
+            }
+    }
+
+
+    init {
+        //add interceptor
+        val okHttpClientBuilder = OkHttpClient.Builder()
+        okHttpClientBuilder.addInterceptor(TheMovieDBInterceptor())
+
+        val client = okHttpClientBuilder.build()
+
+        //construir cliente retrofit
+        retrofit = Retrofit.Builder()
+                .baseUrl(Constants.CINE_BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build()
+        //instanciamos
+        theMovieDBService = retrofit.create(TheMovieDBService::class.java)
+    }
+
+    fun getTheMovieDBService() = theMovieDBService
+
+}
